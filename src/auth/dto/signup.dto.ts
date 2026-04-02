20d8ca662a02890aef501ce8311e87e2dbc1575b
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-02 23:49:10 +0600

    Fixed some typo and added improvement to signup dto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -2,11 +2,11 @@ import { z } from "zod";
 import { Role, Status } from "@prisma/client";
 
 export const signupZodSchema = z.object({
-    fullname: z.string().min(3),
-    email: z.email(),
+    fullname: z.string().trim().min(3),
+    email: z.string().trim().toLowerCase().email(),
     password: z.string().min(8).max(128),
     role: z.nativeEnum(Role),
-    status: z.nativeEnum(Status)
+    status: z.nativeEnum(Status).default(Status.active)
 });
 
-export type signupZodSchemaDto = z.infer<typeof signupZodSchema>;
+export type SignupZodSchemaDto = z.infer<typeof signupZodSchema>;
