927e528e3929d1744f434795044196178b977012
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-02 23:57:51 +0600

    Made some changes to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import config from "./config/configuration";
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+      envFilePath: `.env`,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // App config
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI });
+
+  // .env variable
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+
+  // listening the app
+  await app.listen(port);
+  Logger.log(`Server is running on http://localhost:${port}`);
 }
 bootstrap();
