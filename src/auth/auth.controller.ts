dbebf0a8ee63dbfc5432ef8825c658596cfe9aa9
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-04 15:15:27 +0600

    Added signin router

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, InternalServerErrorException, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, InternalServerErrorException, Post, Res, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
 import { signupZodSchema, type SignupZodSchemaDto } from './dto/signup.dto';
+import { type Response } from 'express';
+import { signinZodSchema, type SigninZodSchemaDto } from './dto/signin.dto';
 
 @Controller({ path: "auth", version: '1' })
 export class AuthController {
@@ -20,6 +22,44 @@ export class AuthController {
         success: true,
         message: "user created successfully",
         data: user,
+        error: null
+      }
+    } catch (error) {
+      throw error instanceof HttpException
+      ? error
+      : new InternalServerErrorException('Failed to create user');
+    }
+  }
+
+  @Post('/signin')
+  @HttpCode(HttpStatus.OK)
+  async signin(
+    @Res({ passthrough: true }) res: Response,
+    @Body(new ZodValidationPipe(signinZodSchema))
+    body: SigninZodSchemaDto
+  ) {
+    try {
+      const token = await this.authService.signin(body);
+
+      // Set token in cookie
+      res.cookie('access_token', token.accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 60000 * 15, // minutes
+      });
+
+      res.cookie('refresh_token', token.refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 3600000 * 24 * 30, // 1 month
+      });
+
+      return {
+        success: true,
+        message: "Logged in successfully",
+        data: null,
         error: null
       }
     } catch (error) {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,23 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignupZodSchemaDto } from './dto/signup.dto';
 import bcrypt from "bcryptjs";
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+import { SigninZodSchemaDto } from './dto/signin.dto';
+import jwt from "jsonwebtoken";
+import { ConfigService } from '@nestjs/config';
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
     constructor(
-    private prisma: PrismaService
+    private prisma: PrismaService,
+    private configService: ConfigService
   ) {}
+
     async signup(
         data: SignupZodSchemaDto
-    ) {
+    ): Promise<{ id: string, fullname: string, email: string }> {
         try {
             const hashPassword = await bcrypt.hash(data.password, 10);
 
@@ -32,6 +38,78 @@ export class AuthService {
                     throw new ConflictException('user already exists');
                 }
             }
+            throw error;
+        }
+    }
+
+    private signJwtAccessToken(
+        payload: { userId: string, userRole: Role }
+    ): string {
+        const JWT_SECRET = this.configService
+            .get<string>('JWT_SECRET_FOR_ACCESS_TOKEN') || "e12fed28-973a-4a80-a324-01bc12f8deeb";
+            
+        return jwt.sign(
+            payload,
+            JWT_SECRET,
+           { expiresIn: '15m'},
+        );
+    }
+
+    private signJwtRefreshToken(
+        payload: { userId: string, userRole: Role }
+    ): string {
+        const JWT_SECRET = this.configService
+            .get<string>('JWT_SECRET_FOR_REFRESH_TOKEN') || "e12fed28-973a-4a80-a324-01bc12f8deeb";
+            
+        return jwt.sign(
+            payload,
+            JWT_SECRET,
+           { expiresIn: '30d'},
+        );
+    }
+
+    async signin(
+        data: SigninZodSchemaDto
+    ): Promise<{ accessToken: string, refreshToken: string }> {
+
+        try {
+            const user = await this.prisma.user.findUnique({
+                where: {
+                    email: data.email,
+                    status: "active"
+                },
+                select: {
+                    id: true,
+                    password: true,
+                    role: true,
+                }
+            });
+
+            if(!user) {
+                throw new NotFoundException('invalid credentials');
+            }
+
+            const isPasswordMatch = await bcrypt.compare(data.password, user.password);
+
+            if(!isPasswordMatch) {
+                throw new BadRequestException('invalid credentials');
+            }
+
+            const accessToken = this.signJwtAccessToken({
+                userId: user.id,
+                userRole: user.role
+            });
+
+            const refreshToken = this.signJwtRefreshToken({
+                userId: user.id,
+                userRole: user.role
+            });
+
+            return {
+                accessToken,
+                refreshToken
+            }
+        } catch (error) {
             throw error;
         }
     }
