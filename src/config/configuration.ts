301863728e0f0773d4312eae7c07c8ac97933f5f
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-04 22:20:44 +0600

    Added env variable to config file

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,4 +3,6 @@ import 'dotenv/config';
 export default () => ({
   DATABASE_URL: process.env.DATABASE_URL,
   PORT: parseInt(process.env.PORT!, 10) || 3000,
+  JWT_SECRET_FOR_REFRESH_TOKEN: process.env.JWT_SECRET_FOR_REFRESH_TOKEN,
+  JWT_SECRET_FOR_ACCESS_TOKEN: process.env.JWT_SECRET_FOR_ACCESS_TOKEN
 });
