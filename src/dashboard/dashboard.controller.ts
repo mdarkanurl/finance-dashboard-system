3ed0e1b49fa819fc597ddc63868f128275e601a5
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-05 11:58:19 +0600

    Added /categories router

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -15,6 +15,26 @@ export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
   @Roles(Role.viewer, Role.analyst, Role.admin)
+  @Get('/categories')
+  @HttpCode(HttpStatus.OK)
+  async getCategories() {
+    try {
+      const categories = await this.dashboardService.getCategories();
+
+      return {
+        success: true,
+        message: 'dashboard categories fetched successfully',
+        data: categories,
+        error: null,
+      };
+    } catch (error) {
+      throw error instanceof HttpException
+        ? error
+        : new InternalServerErrorException('Failed to fetch dashboard categories');
+    }
+  }
+
+  @Roles(Role.viewer, Role.analyst, Role.admin)
   @Get('/summary')
   @HttpCode(HttpStatus.OK)
   async getSummary() {
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -6,6 +6,46 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class DashboardService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async getCategories() {
+    const where = {
+      deletedAt: null,
+    };
+
+    const [categories, totalAmountSummary] = await this.prisma.$transaction([
+      this.prisma.record.groupBy({
+        by: ['category'],
+        where,
+        _sum: {
+          amount: true,
+        },
+        orderBy: {
+          _sum: {
+            amount: 'desc',
+          },
+        },
+      }),
+      this.prisma.record.aggregate({
+        where,
+        _sum: {
+          amount: true,
+        },
+      }),
+    ]);
+
+    const totalAmount = totalAmountSummary._sum.amount ?? 0;
+
+    return categories.map((item) => {
+      const categoryTotalAmount = item._sum?.amount ?? 0;
+
+      return {
+        category: item.category,
+        totalAmount: categoryTotalAmount,
+        percentage:
+          totalAmount === 0 ? 0 : Number(((categoryTotalAmount / totalAmount) * 100).toFixed(2)),
+      };
+    });
+  }
+
   async getSummary() {
     const where = {
       deletedAt: null,
