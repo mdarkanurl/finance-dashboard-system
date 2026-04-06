163b066c9babf0c169d3a399ee116cb38d965d75
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-06 09:02:49 +0600

    Added .env.example

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,5 +4,6 @@ export default () => ({
   DATABASE_URL: process.env.DATABASE_URL,
   PORT: parseInt(process.env.PORT!, 10) || 3000,
   JWT_SECRET_FOR_REFRESH_TOKEN: process.env.JWT_SECRET_FOR_REFRESH_TOKEN,
-  JWT_SECRET_FOR_ACCESS_TOKEN: process.env.JWT_SECRET_FOR_ACCESS_TOKEN
+  JWT_SECRET_FOR_ACCESS_TOKEN: process.env.JWT_SECRET_FOR_ACCESS_TOKEN,
+  REDIS_URL: process.env.REDIS_URL
 });
